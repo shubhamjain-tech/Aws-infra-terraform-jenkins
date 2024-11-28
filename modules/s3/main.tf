5bf3c529e86af9225b9fca873bfae661348e156c
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-11-28 13:54:12 +0530

    Initial commit with Terraform code for dev-feature branch

---
+++ modules/s3/main.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  tags = {
+    Name = var.bucket_name
+    Environment = var.environment
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"  # Default region
+}
