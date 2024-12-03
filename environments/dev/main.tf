b46e0068f4b682884ec274390984c05449c6c235
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-12-03 12:02:29 +0530

    new vpc added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -55,11 +55,11 @@ module "s3" {
   environment = "dev"
 }
 
-# module "vpc" {
-#   source          = "../../modules/vpc"
-#   environment     = "dev"
-#   cidr_block      = "10.0.0.0/16"
-#   public_subnet_cidr = "10.0.1.0/24"
-#   private_subnet_cidr = "10.0.2.0/24"
-#   availability_zone   = "ap-south-1"
-# }
+module "vpc" {
+  source                = "./vpc-module"
+  vpc_cidr              = "10.0.0.0/16"
+  vpc_name              = "my-vpc"
+  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs  = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones    = ["ap-south-1a", "ap-south-1b"]
+}
