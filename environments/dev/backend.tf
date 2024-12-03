63504c03c0a8732636d2b0321102b4c68f10a8ca
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-12-03 14:17:44 +0530

    feature-prod updated

--- environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mybucket-backend-6721"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
