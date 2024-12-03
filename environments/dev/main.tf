79949d188cbe365a8bc7e1b12c76750df5221f79
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-12-03 12:23:59 +0530

    new vpc added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -59,7 +59,7 @@ module "s3" {
 }
 
 module "vpc" {
-  source                = "./vpc-module"
+  source                = "../../modules/vpc"
   vpc_cidr              = "10.0.0.0/16"
   vpc_name              = "my-vpc"
   public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
