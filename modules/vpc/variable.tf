debfa99273d65fc78585158e9558ba9310465b23
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-12-03 11:04:01 +0530

    vpc block comment

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,28 +1,28 @@
-variable "environment" {
-  description = "dev"
-  type        = string
-}
+# variable "environment" {
+#   description = "dev"
+#   type        = string
+# }
 
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
+# variable "cidr_block" {
+#   description = "CIDR block for the VPC"
+#   type        = string
+#   default     = "10.0.0.0/16"
+# }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
+# variable "public_subnet_cidr" {
+#   description = "CIDR block for the public subnet"
+#   type        = string
+#   default     = "10.0.1.0/24"
+# }
 
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
-  type        = string
-  default     = "10.0.2.0/24"
-}
+# variable "private_subnet_cidr" {
+#   description = "CIDR block for the private subnet"
+#   type        = string
+#   default     = "10.0.2.0/24"
+# }
 
-variable "availability_zone" {
-  description = "Availability Zone for the subnets"
-  type        = string
-  default     = "ap-south-1"
-}
+# variable "availability_zone" {
+#   description = "Availability Zone for the subnets"
+#   type        = string
+#   default     = "ap-south-1"
+# }
