4a8cb2ea786d3e6bb205f3d36406e3bb0255235d
shubhamjain-tech <shubhamjain9005@outlook.com>
2024-12-03 11:08:33 +0530

    vpc block comment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -28,11 +28,11 @@ module "ec2" {
       name          = "dev-instance-2"
     },
     # Add more instances as needed
-    # instance3 = {
-    #   ami_id        = "ami-0dee22c13ea7a9a67"
-    #   instance_type = "t2.micro"
-    #   name          = "dev-instance-3"
-    # }
+    instance4 = {
+      ami_id        = "ami-0dee22c13ea7a9a67"
+      instance_type = "t2.micro"
+      name          = "dev-instance-4"
+    }
   }
   environment = "dev"
 }
